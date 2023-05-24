38dbe9dbee9447c5c5cdd5f54fe76f3ad72eb8d1
Manasa sadasiva <manasasadasiva@gmail.com>
2023-05-23 21:59:45 -0700

    program1
    package name

--- src/dATATYPES/Datatypes.java
+++ src/dATATYPES/Datatypes.java
@@ -4,7 +4,24 @@ public class Datatypes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//classification of datatypes
+		//1.primitive datatype(fixed memory)
+		//1.1 boolean type(condition basis operation)true or false
+		//1.2 number type
+		//1.2.1 char value only ascii format american standard code for information interchange
+		//this table to used for coding its stored as char value in single quotes 'a';
+		//1.2.2 integral value
+		//1.2.2.1 integer
+		//byte (stores 8bit value)
+		//short(16bit value)
+		//int(32 bit vaiue)
+		//long(64 bit value)
+		//1.2.2.2 floating value(decimal values
+		//float(32 bit value)after dot u can store 7 decimal points
+		//double(64 bit vaule)16 decimal values
+		
+		//2.nonprimitive dataype(not fixed memory)
+		//ex:string array 
 	}
 
 }
--- src/day01/Program1.java
+++ src/day01/Program1.java
@@ -3,7 +3,17 @@ package day01;
 public class Program1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 TODO Auto-generated method stub
+	while creating package name
+		we should start with alphabet
+		we should not start with numeric
+		we can make it alphanumeric
+		no special char other than $ _
+		we can use $ _ even in te begining
+		even space is considered as special char we shdnt use
+		we shd not use keywords as package or class names which already defined 
+		package name starts with lower case 
+			class name starts with upper case
 
 	}
 
